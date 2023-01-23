b98d3b2346585bdd520c0f45420fbd6bf3244773
borja <borja@civir.es>
2023-01-23 15:47:20 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+
+
+}
+resource "aws_instance" "borja-server" {
+  ami           = "ami-00874d747dde814fa"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Borja-server"
+  }
+}
+
