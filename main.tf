45f2c84ac4c99b85d9b4f8e722ae155b180e776b
borja <borja@civir.es>
2023-01-24 15:21:57 +0000

    add main.tf secgroup

--- main.tf
+++ main.tf
@@ -10,5 +10,27 @@ resource "aws_instance" "borja-server" {
   tags = {
     Name = "Borja-server"
   }
+  user_data                   = <<-EOF
+    #!/bin/bash
+    echo "Hello World" > index.html
+    nohup busybox httpd -f -p 8080 &
+  EOF
+  user_data_replace_on_change = true
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
 }
+
+resource "aws_security_group" "instance" {
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+
+}
+
 
